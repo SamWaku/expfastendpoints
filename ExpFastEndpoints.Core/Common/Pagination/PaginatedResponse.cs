8476f58c50ff24c71194ea45b69cced5f4d2e97a
SamWaku <samuelwaku1st@gmail.com>
2025-06-04 15:49:03 +0200

    chore: paginated response

---
+++ ExpFastEndpoints.Core/Common/Pagination/PaginatedResponse.cs
@@ -0,0 +1,19 @@
+﻿namespace ExpFastEnpoints.ExpFastEndpoints.Core.Common.Pagination;
+
+public interface IPaginatedResponse<T>
+{
+    int Page { get; set; }
+    int PageSize { get; set; }
+    int Total { get; set; }
+    int TotalPages { get; set; }
+    List<T> Data { get; set; }
+}
+
+public class PaginatedResponse<T> : IPaginatedResponse<T>
+{
+    public required int Page { get; set; }
+    public required int PageSize { get; set; }
+    public required int Total { get; set; }
+    public required int TotalPages { get; set; }
+    public required List<T> Data { get; set; } = [];
+}
