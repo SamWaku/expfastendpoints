efbe347f1eee881d853d1ff0b999d9c4f69f0b5a
SamWaku <samuelwaku1st@gmail.com>
2025-06-04 12:28:53 +0200

    chore: create investment house

---
+++ ExpFastEndpoints.Core/Endpoints/CreateInvestmentHouseEndpoint.cs
@@ -0,0 +1,13 @@
+﻿using FastEndpoints;
+
+namespace ExpFastEnpoints.ExpFastEndpoints.Core.Endpoints;
+
+public class CreateInvestmentHouseEndpoint : Endpoint<InvestmentHouseRequest>
+{
+    public override void Configure()
+    {
+        Post("exp/create-investment-house");
+        Summary(s => s.Summary = "Create Investment House");
+        AllowAnonymous();
+    }
+}
---
+++ ExpFastEndpoints.Core/Entities/InvestmentHouseRequest.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+public class InvestmentHouseRequest 
+{
+    public string? CompanyName {get;set;}
+    public string? InstitutionType {get;set;}
+    public string? CompanyRegistrationNumber {get;set;}
+    public string? Tpin {get;set;}
+    public string? CountryOfIncorporation {get;set;}
+    public DateOnly DateOfIncorporation {get;set;}
+    public string? PhysicalAddress {get;set;}
+    public string? PostalAddress {get;set;}
+    public string? TelephoneNumber {get;set;}
+    public string? MobileNumber {get;set;}
+    [EmailAddress]
+    public string? EmailAddress {get;set;}
+    protected List<ContactPerson> ContactPersons { get; set; } = new();
+    protected List<Director> Directors { get; set; } = new();
+    public Boolean? CertificateOfIncorporation {get;set;}
+    public Boolean? TaxClearanceCertificate {get;set;}
+    public Boolean? TradingLicense {get;set;}
+    public Boolean? Financials {get;set;}
+    
+    protected class ContactPerson
+    {
+        public string? Name { get; set; }
+        public string? Phone { get; set; }
+        [EmailAddress]
+        public string? Email { get; set; }
+        public string? Position { get; set; }
+    }
+    
+    protected class Director
+    {
+        public string? Name { get; set; }
+        public string? Phone { get; set; }
+        [EmailAddress]
+        public string? Email { get; set; }
+        public string? Position { get; set; }
+    }
+}
