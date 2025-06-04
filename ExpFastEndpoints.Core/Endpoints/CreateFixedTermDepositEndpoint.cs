c985bc82a6ae1b55b3eff8b6f9be498f8210a9cc
SamWaku <samuelwaku1st@gmail.com>
2025-06-04 11:55:54 +0200

    chore: create fixed deposit save

--- ExpFastEndpoints.Core/Endpoints/CreateFixedTermDepositEndpoint.cs
+++ ExpFastEndpoints.Core/Endpoints/CreateFixedTermDepositEndpoint.cs
@@ -1,6 +1,34 @@
-﻿namespace ExpFastEnpoints.ExpFastEndpoints.Core.Endpoints;
+﻿using ExpFastEnpoints.ExpFastEndpoints.Core.Database;
+using ExpFastEnpoints.ExpFastEndpoints.Core.Models;
+using FastEndpoints;
 
-public class CreateFixedTermDepositEndpoint
+namespace ExpFastEnpoints.ExpFastEndpoints.Core.Endpoints;
+
+public class CreateFixedTermDepositEndpoint(PostgresDatabase postgresDb) : Endpoint<FixedTermDepositRequest>
 {
-    
+    public override void Configure()
+    {
+        Post("exp/create-fixed-term-deposit");
+        Summary(s => s.Summary = "Create Fixed Term Deposit");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(FixedTermDepositRequest req, CancellationToken ct)
+    {
+        var database = postgresDb;
+        var newFixedTermDeposit = new FixedTermDeposit
+        {
+            InvestmentHouseId = req.InvestmentHouseId,
+            Amount = req.Amount,
+            InterestRate = req.InterestRate,
+            Tenure = req.Tenure,
+            StartDate = req.StartDate,
+            MaturityDate = req.MaturityDate,
+            InterestAmount = req.InterestAmount,
+            MaturityAmount = req.MaturityAmount,
+        };
+
+        database.Add(newFixedTermDeposit);
+        await database.SaveChangesAsync(ct);
+    }
 }
--- ExpFastEndpoints.Core/Entities/FixedTermDepositRequest.cs
+++ ExpFastEndpoints.Core/Entities/FixedTermDepositRequest.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using ExpFastEnpoints.ExpFastEndpoints.Core.Models;
+
+public class FixedTermDepositRequest
+{
+    public int Id { get; set; }
+
+    public int InvestmentHouseId { get; set; }
+
+    public double Amount { get; set; }
+
+    public double InterestRate { get; set; }
+
+    public int Tenure { get; set; }
+
+    public DateOnly StartDate { get; set; }
+
+    public DateOnly MaturityDate { get; set; }
+
+    public double InterestAmount { get; set; }
+
+    public double MaturityAmount { get; set; }
+
+    public DateTime DateCreated { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public virtual InvestmentHouse InvestmentHouse { get; set; } = null!;
+}
