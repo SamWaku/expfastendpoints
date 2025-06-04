2c2f70a8e9fd0017789ef0faf82faddad271a567
SamWaku <samuelwaku1st@gmail.com>
2025-06-04 11:36:52 +0200

    chore: fixedtermdeposit table

---
+++ ExpFastEndpoints.Core/Models/FixedTermDeposit.cs
@@ -0,0 +1,30 @@
+﻿using ExpFastEnpoints.ExpFastEndpoints.Core.Models;
+
+namespace ExpFastEnpoints.ExpFastEndpoints.Core.Models;
+
+public partial class FixedTermDeposit
+{
+    public int Id { get; set; }
+
+    public int InvestmentHouseId { get; set; }
+
+    public double Amount { get; set; }
+
+    public double InterestRate { get; set; }
+
+    public int Tenure { get; set; }
+
+    public DateOnly StartDate { get; set; }
+
+    public DateOnly MaturityDate { get; set; }
+
+    public double InterestAmount { get; set; }
+
+    public double MaturityAmount { get; set; }
+
+    public DateTime DateCreated { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public virtual InvestmentHouse InvestmentHouse { get; set; } = null!;
+}
