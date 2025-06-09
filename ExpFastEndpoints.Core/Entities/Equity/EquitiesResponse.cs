e8a375ad0acc738c0c978f7a9e816164a11cbd4c
SamWaku <samuelwaku1st@gmail.com>
2025-06-09 11:33:10 +0200

    chore: equities response

--- ExpFastEndpoints.Core/Entities/Equity/EquitiesResponse.cs
+++ ExpFastEndpoints.Core/Entities/Equity/EquitiesResponse.cs
@@ -1,6 +1,12 @@
-﻿namespace ExpFastEnpoints.ExpFastEndpoints.Core.Entities.Equity;
+﻿using ExpFastEnpoints.ExpFastEndpoints.Core.Common;
+using ExpFastEnpoints.ExpFastEndpoints.Core.Models;
 
-public class EquitiesResponse
+namespace ExpFastEnpoints.ExpFastEndpoints.Core.Entities.Equity;
+
+public class EquitiesResponse: BaseEntity
 {
-    
+    public OrderType OrderType { get; set; }
+    public string? Company { get; set; }
+    public double Quantity { get; set; }
+    public double SharePrice { get; set; }
 }
