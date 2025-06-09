efb89cf090fc95dc6371c91f18d55c2ec70f7f86
SamWaku <samuelwaku1st@gmail.com>
2025-06-09 12:26:14 +0200

    chore: create commercial paper request

---
+++ ExpFastEndpoints.Core/Entities/CommercialPaper/CreateCommercialPaperRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace ExpFastEnpoints.ExpFastEndpoints.Core.Entities.CommercialPaper;
+
+public class CreateCommercialPaperRequest
+{
+    public required string CompanyName { get; set; }
+    public double InvestedAmount { get; set; }
+    public double InterestRate { get; set; }
+    public int Tenure { get; set; }
+    public DateOnly StartDate { get; set; }
+    public DateOnly MaturityDate { get; set; }
+}
