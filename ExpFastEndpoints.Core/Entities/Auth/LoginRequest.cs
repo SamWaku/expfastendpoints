c99404de801e9121716473d7078c141d37109ed2
SamWaku <samuelwaku1st@gmail.com>
2025-06-12 12:53:48 +0200

    chore: login request

---
+++ ExpFastEndpoints.Core/Entities/Auth/LoginRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpFastEnpoints.ExpFastEndpoints.Core.Entities.Auth;
+
+public class LoginRequest
+{
+    public string Name { get; set; }
+    [EmailAddress]
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
